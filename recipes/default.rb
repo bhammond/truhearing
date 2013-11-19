0786f7980eda30fc22373c21553e63e2a2f9f17e
Brett Hammond <bretthammond@stubthumb.com>
2013-11-19 09:56:16 -0700

    Updated Recipe

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,63 +1,70 @@
 pg_user "securetruhearing" do
-  privileges :superuser => true, :createdb => true, :login => true
-  password "tXTV2aYqmdc7"
+    privileges :superuser => true, :createdb => true, :login => true
+    password "tXTV2aYqmdc7"
 end
 
 pg_database "securetruhearing_sprint5" do
-  owner "securetruhearing"
-  encoding "utf8"
-  template "template0"
-  locale "en_US.UTF8"
+    owner "securetruhearing"
+    encoding "utf8"
+    template "template0"
+    locale "en_US.UTF8"
 end
 
 pg_database_extensions "securetruhearing_sprint5" do
-  languages ["plpgsql", "plv8"]             # install `plpgsql` language - single value may be passed without array
-  # extensions ["hstore"]  # install `hstore` and `dblink` extensions - multiple values in array
-  # postgis true                     # install `postgis` support
+    languages ["plpgsql", "plpythonu"]
+    extensions ["hstore"]
 end
 
-# using apt install some packages
+pg_database_extensions "postgres" do
+    languages ["plpythonu"]
+    extensions ["hstore"]
+end
+
 package "php5-pgsql" do
-  action :install
+    action :install
+end
+
+package "postgresql-contrib" do
+    action :install
 end
 
 package "postgresql-client-common" do
-  action :install
+    action :install
 end
 
-package "postgresql-plpython-9.1" do
-  action :install
+package "postgresql-plpython-9.3" do
+    action :install
 end
 
 package "smbfs" do
-  action :install
+    action :install
 end
 
 package "php5-cli" do
-  action :install
+    action :install
 end
 
 package "php5-curl" do
-  action :install
+    action :install
 end
 
 package "php5-intl" do
-  action :install
+    action :install
 end
 
 package "php5-mcrypt" do
-  action :install
+    action :install
 end
 
 package "python-cjson" do
-  action :install
+    action :install
 end
 
 include_recipe "apache2"
 
 web_app "t3" do
-  server_name "t3.dev"
-  server_aliases ["www.t3.dev"]
-  docroot "/var/www/t3/src/t3/"
+    server_name "t3.dev"
+    server_aliases ["www.t3.dev"]
+    docroot "/var/www/t3/src/t3/"
 end
 
